72ca5f452f64fad69c0d335ff03151b781fe4413
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-06-11 22:23:13 -0400

    Try this

--- packages/@ember/template-compilation.js
+++ packages/@ember/template-compilation.js
@@ -1,4 +1,4 @@
-import * as emberTemplateCompiler from "https://esm.sh/*ember-source@5.9.0/dist/ember-template-compiler.js";
+import emberTemplateCompiler from "https://esm.sh/*ember-source@5.9.0/dist/ember-template-compiler.js";
 
 export const compileTemplate = emberTemplateCompiler.compile;
 export const precompileTemplate = emberTemplateCompiler.precompile;
